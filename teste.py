afb7ecc508681fd0b368f3ad8849b7305f93862c
victor <victorhugoocordeirorocha@gmail.com>
2023-10-29 22:21:26 -0400

    Escolhendo uma palavra aleatória

--- teste.py
+++ teste.py
@@ -2,28 +2,46 @@ import random
 import sys
 
 
-def palavras():  # Função para sortea uma palavra
+def palavras():  # Função para sortear uma palavra
 
     # Dicionario contendo todas as palavras
     palavra = {
-        'animal': ['Macaco', 'elefante', 'girafa'],
-        'nome': ['Victor', 'fabio', 'matheus'],
-        'cor': ['Azul', 'amarelo', 'branco', 'rosa', 'roxo'],
+        'animal': ['macaco', 'elefante', 'girafa'],
+        'nome': ['victor', 'fabio', 'matheus'],
+        'cor': ['azul', 'amarelo', 'branco', 'rosa', 'roxo'],
     }
 
     # Sorteando a chave e uma palvra do dict
     chave_sorteada = random.choice(list(palavra.keys()))
     palavra_sorteada = random.choice(palavra[chave_sorteada])
 
-    return print(f'A dica é {chave_sorteada} e a palavra é {palavra_sorteada}')
+    print(f'Dica: {chave_sorteada.upper()}')
+
+    return palavra_sorteada.upper()
 
 
 print(23*'-')
 print((4*' '), 'JOGO DA FORCA')
 print(23*'-')
-
 print('\nObjetivo: Tente adivinhar a palvra gerada.')
-print('Regras: Você só pode digitar uma letra por vez, com o limite de 6 erros. ')
+print('Regras: Você só pode digitar uma letra por vez, com o limite de 6 erro.\n')
 
-print()
-palavras()
+loop = True
+
+# while loop:
+palavra = palavras()
+palavra_letras = []
+
+print('A palavra sorteada é... ')
+for letra in palavra:
+    print('_', end=' ')
+    palavra_letras.append(letra)
+
+print(palavra)
+print('\n')
+palpite = input('Digite seu palpite <<< ').upper()
+
+if palpite in palavra:
+    print('Ok')
+else:
+    print('erro')
