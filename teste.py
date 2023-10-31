2c197057eed17c66703af79b82daf9fdd63ee94a
victor <victorhugoocordeirorocha@gmail.com>
2023-10-30 22:24:28 -0400

    Testando

--- teste.py
+++ teste.py
@@ -2,13 +2,14 @@ import random
 import sys
 
 
-def palavras():  # Função para sortear uma palavra
+def palavras_sorteada():  # Função para sortear uma palavra
 
     # Dicionario contendo todas as palavras
     palavra = {
         'animal': ['macaco', 'elefante', 'girafa'],
         'nome': ['victor', 'fabio', 'matheus'],
         'cor': ['azul', 'amarelo', 'branco', 'rosa', 'roxo'],
+        'fruta': ['abacaxi', 'uva', 'banana'],
     }
 
     # Sorteando a chave e uma palvra do dict
@@ -27,10 +28,9 @@ print('\nObjetivo: Tente adivinhar a palvra gerada.')
 print('Regras: Você só pode digitar uma letra por vez, com o limite de 6 erro.\n')
 
 loop = True
-
-# while loop:
-palavra = palavras()
+palavra = palavras_sorteada()
 palavra_letras = []
+palpites_certo = []
 
 print('A palavra sorteada é... ')
 for letra in palavra:
@@ -39,11 +39,16 @@ for letra in palavra:
 
 print(palavra)
 print('\n')
-palpite = input('Digite seu palpite <<< ').upper()
 
-while not palavra.issubset(palpite):
-    if palpite in palavra:
-        print('Ok')
-    else:
-        print('erro')
+while loop:
+
     palpite = input('Digite seu palpite <<< ').upper()
+
+    palpites_certo.append(palpite)
+
+    for letras in palavra_letras:
+        if palpite == letras:
+            print(palpite, end=' ')
+
+        else:
+            print('_', end=' ')
