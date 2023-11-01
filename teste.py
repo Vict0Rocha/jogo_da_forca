6681e512050e5fbb66e4200a1ef783dd15fe19a9
victor <victorhugoocordeirorocha@gmail.com>
2023-10-31 22:16:31 -0400

    Só passando raiva por enquanto, nada de funcionar

--- teste.py
+++ teste.py
@@ -21,7 +21,7 @@ def palavras_sorteada():  # Função para sortear uma palavra
     return palavra_sorteada.upper()
 
 
-def boneco_forca(chances_retantes):
+def boneco_forca(chances_retantes):  # Função com uma lista com o desenho do boneco
     boneco = [
         '''
     ------
@@ -90,34 +90,33 @@ def boneco_forca(chances_retantes):
     return boneco[chances_retantes]
 
 
-# print(23*'-')
-# print((4*' '), 'JOGO DA FORCA')
-# print(23*'-')
-# print('\nObjetivo: Tente adivinhar a palvra gerada.')
-# print('Regras: Você só pode digitar uma letra por vez, com o limite de 6 erro.\n')
+print(23*'-')
+print((4*' '), 'JOGO DA FORCA')
+print(23*'-')
+print('\nObjetivo: Tente adivinhar a palvra gerada.')
+print('Regras: Você só pode digitar uma letra por vez, com o limite de 6 erros.\n')
 
-# loop = True
-# palavra = palavras_sorteada()
+loop = True
+palavra = palavras_sorteada()
 # palavra_letras = []
-# palpites_certo = []
+palpites_certo = []
+conjunto_palavra = [palavra]
 
-# print('A palavra sorteada é... ')
-# for letra in palavra:
-#     print('_', end=' ')
-#     palavra_letras.append(letra)
+print('A palavra sorteada é... ')
+espacos = '_ ' * len(palavra)
+print(espacos)
 
-# print(palavra)
-# print('\n')
+print(palavra)
+print('\n')
 
-# while loop:
+while conjunto_palavra == palpites_certo:
 
-#     palpite = input('Digite seu palpite <<< ').upper()
+    palpite = input('Digite seu palpite <<< ').upper()
 
-#     palpites_certo.append(palpite)
-
-#     for letras in palavra_letras:
-#         if palpite == letras:
-#             print(palpite, end=' ')
-
-#         else:
-#             print('_', end=' ')
+    if palpite in conjunto_palavra:
+        palpites_certo.append(palpite)
+        for letras in palavra:
+            if palpite == letras:
+                print(palpite, end=' ')
+            else:
+                print('_', end=' ')
